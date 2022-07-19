28069c90a12d9bda76aa718d23474009144c8a46
rm1118s <Lumia950>
2022-07-19 11:37:43 -0400

    Upload page

--- frontend/frontend/src/app/app-routing.module.ts
+++ frontend/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./main/main.component";
+import {UploadComponent} from "./upload/upload.component";
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'upload', component: UploadComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- frontend/frontend/src/app/upload/upload.component.ts
+++ frontend/frontend/src/app/upload/upload.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {map, Observable, startWith} from "rxjs";
 
 @Component({
   selector: 'app-upload',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  team = new FormControl('');
+  filteredOptions: Observable<string[]>;
+  teams = ['team a', 'team b', 'team c']
+  constructor() {
+    this.filteredOptions = this.team.valueChanges.pipe(
+      startWith(''),
+      map((value: string) => this._filter(value || '')),
+    );
   }
 
+  ngOnInit(): void {
+
+  }
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.teams.filter((option: string) => option.toLowerCase().includes(filterValue));
+  }
 }
