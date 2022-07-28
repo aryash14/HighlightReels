7849399d95b2c95e91d1efa98f49962a2a5258da
rm1118s <Lumia950>
2022-07-28 16:37:37 -0400

    filter select sport and team

--- frontend/frontend/src/app/main/main.component.ts
+++ frontend/frontend/src/app/main/main.component.ts
@@ -20,24 +20,32 @@ export class MainComponent implements OnInit {
   filteredOptions: Observable<string[]>;
 
   teams: any;
+  team_names = [];
   search = new UntypedFormControl('');
   example_highlight: Highlight;
   public example_highlight_arr: (Highlight | any)[];
   ptr = 0
   @ViewChild(PlayerComponent) child: PlayerComponent | undefined;
 
-  sport_index = 0;
+  sport_selected_id = new UntypedFormControl('');
   sport_team: any;
+  filter_team: any;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, public domSanitizer: DomSanitizer) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.search = new UntypedFormControl('');
-
     this.filteredOptions = this.search.valueChanges.pipe(
       startWith(''),
       map((value: string) => this._filter(value || '')),
     );
+
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.search = new UntypedFormControl('');
+
+
+    this.sport_selected_id.valueChanges.subscribe( sport_id => {
+        this.get_filtered_teams(sport_id)
+      }
+    )
 
     this.example_highlight_arr = [
       new Highlight('Al Horford PLAYOFF CAREER HIGH!', 'Celtics beat the Warriors!',
@@ -55,19 +63,39 @@ export class MainComponent implements OnInit {
     this.example_highlight = this.example_highlight_arr[this.ptr]
   }
 
+  private get_filtered_teams(sport_id: any) {
+
+    let team = [];
+    this.team_names = []
+    for (const x of this.sport_team) {
+      if (x._id === sport_id) {
+        team = x.team;
+      }
+    }
+    for (const x of team) {
+      // @ts-ignore
+      this.team_names.push(x.name)
+    }
+
+    this.filteredOptions = this.search.valueChanges.pipe(
+      startWith(''),
+      map((value: string) => this._filter(value || '')),
+    );
+  }
+
   private async getSportTeam() {
     return (await Axios.get("http://localhost:3000/sportteam")).data;
   }
 
   async ngOnInit(): Promise<any> {
-    this.teams = ['team a', 'team b', 'team c']
+
     this.sport_team = await this.getSportTeam()
     console.log(this.sport_team)
   }
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-    return this.teams.filter((option: string) => option.toLowerCase().includes(filterValue));
+    return this.team_names.filter((option: string) => option.toLowerCase().includes(filterValue));
   }
 
   next_arrow(): void {
